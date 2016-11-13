34427e4e19e20e4934339cbc5f4da07f0e2ecca3
mariviit <mariviit@greeny.cs.tlu.ee>
2016-11-13 10:08:40 +0200

    lisatud päis, jalus, otsing ja sorteerimine

---
+++ functions.php
@@ -0,0 +1,61 @@
+<?php
+
+	require("/home/mariviit/config.php");
+	
+	session_start();
+	
+	//ühendus
+	$database = "if16_mariiviita";
+	$mysqli = new mysqli($serverHost, $serverUsername, $serverPassword, $database);
+	
+	//require("User.class.php");
+	//$User=new User($mysqli);
+	
+	//require("Car.class.php");
+	//$Car=new Car($mysqli);
+	
+	//require("Interest.class.php");
+	//$Interest=new Interest($mysqli);
+	
+	//require("Helper.class.php");
+	//$Helper=new Helper();
+	
+	// functions.php
+	//var_dump($GLOBALS);
+	
+	// see fail, peab olema kõigil lehtedel kus 
+	// tahan kasutada SESSION muutujat
+	
+	
+	//***************
+	//**** SIGNUP ***
+	//***************
+	
+
+	
+
+
+	
+	
+	
+	/*function sum($x, $y) {
+		
+		return $x + $y;
+		
+	}
+	
+	
+	function hello($firsname, $lastname) {
+		
+		return "Tere tulemast ".$firsname." ".$lastname."!";
+		
+	}
+	
+	echo sum(5123123,123123123);
+	echo "<br>";
+	echo hello("Romil", "Robtsenkov");
+	echo "<br>";
+	echo hello("Juku", "Juurikas");
+	*/
+
+?>
